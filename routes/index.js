a5678b021dffcd25165c9c68192f9496ec233def
Briebestcode30 <brieanaharris033@penguin.com>
2026-01-29 19:17:17 -0500

    Add catch-all 404 handler to main router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const router = require("express").Router();
+const { NOT_FOUND_ERROR_CODE } = require("../utils/errors"); // import the 404 constant
 
 const userRouter = require("./users");
-const clothingItemRouter = require("./clothingItems");
+const clothingItemRouter = require("./clothingItems"); // if created later
 
 router.use("/users", userRouter);
 router.use("/items", clothingItemRouter);
+
+// Catch-all 404 handler for unmatched routes
+router.use((req, res) => {
+  res
+    .status(NOT_FOUND_ERROR_CODE)
+    .send({ message: "Requested resource not found" });
+});
 
 module.exports = router;
